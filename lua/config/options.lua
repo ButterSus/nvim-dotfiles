1dce7d3724513a15e801cef6ad235b0eed42b3b5
Krivoshapkin Eduard <krivoshapkinea@proton.me>
2026-07-24 00:47:49 +0300

    feat: add sensible options
    AI generated by Claude
    haven't tested them out yet

---
+++ lua/config/options.lua
@@ -0,0 +1,25 @@
+local opt = vim.opt
+
+-- Line numbers
+opt.number = true
+opt.relativenumber = true
+
+-- Tabs & Indentation
+opt.tabstop = 4
+opt.shiftwidth = 4
+opt.expandtab = true
+opt.smartindent = true
+
+-- Search & UI
+opt.ignorecase = true
+opt.smartcase = true
+opt.termguicolors = true
+opt.signcolumn = "yes"
+opt.cursorline = true
+
+-- System Clipboard integration
+opt.clipboard = "unnamedplus"
+
+-- Save undo history across Neovim sessions
+opt.undofile = true
+opt.updatetime = 250
