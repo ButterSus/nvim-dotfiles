e2d3044237184fd5cf521054df108bc3b4375320
Krivoshapkin Eduard <krivoshapkinea@proton.me>
2026-07-24 02:54:44 +0300

    fix: mappings comment and description

--- lua/config/mappings.lua
+++ lua/config/mappings.lua
@@ -1,5 +1,8 @@
+-- This file should be 100% compatible with Visual Studio Code
+-- use vim.g.vscode to exclude logic
+
 local map = vim.keymap.set
 
 -- VS Code style Tab / Shift-Tab indenting in Visual Mode
-map("v", "<Tab>", ">gv", { desc = "Indent selection" })
-map("v", "<S-Tab>", "<gv", { desc = "Unindent selection" })
+map("v", "<Tab>", ">gv", { desc = "Indent Selection" })
+map("v", "<S-Tab>", "<gv", { desc = "Unindent Selection" })
