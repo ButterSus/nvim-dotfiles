08c351a648e11066d4b77dd5ab95c7c2ae71b5dd
Krivoshapkin Eduard <krivoshapkinea@proton.me>
2026-07-24 05:40:36 +0300

    fix: replace deprecated diagnostic api

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -41,8 +41,12 @@ return {
           -- vim.keymap.set("n", "<leader>wl", function()
           --   print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
           -- end, opts)
-          vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
-          vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
+          vim.keymap.set("n", "[d", function()
+            vim.diagnostic.jump { count = -1, float = true }
+          end, opts)
+          vim.keymap.set("n", "]d", function()
+            vim.diagnostic.jump { count = 1, float = true }
+          end, opts)
           vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts)
           vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, opts)
         end,
