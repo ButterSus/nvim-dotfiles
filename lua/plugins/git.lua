112cad977066094fb4be617606eb59f69fdc0058
Krivoshapkin Eduard <krivoshapkinea@proton.me>
2026-07-24 05:21:28 +0300

    feat: add git blame
    No additional plugin needed, gitsigns.nvim
    already can do that

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -2,6 +2,13 @@ return {
   {
     "lewis6991/gitsigns.nvim",
     event = { "BufReadPre", "BufNewFile" },
+    opts = {
+      current_line_blame = true,
+      current_line_blame_opts = {
+        -- Show immediately with zero delay, just like VS Code
+        delay = 0,
+      },
+    },
     keys = {
       -- Navigation
       {
