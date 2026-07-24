7d1e94e4ad5a62fe9976723892ea729c0a9718aa
Krivoshapkin Eduard <krivoshapkinea@proton.me>
2026-07-24 05:26:33 +0300

    feat: show indentation levels
    indent-blankline.nvim plugin allows that

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -3,4 +3,16 @@ return {
     "folke/which-key.nvim",
     event = "VeryLazy",
   },
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    main = "ibl",
+    event = { "BufReadPre", "BufNewFile" },
+    ---@module "ibl"
+    ---@type ibl.config
+    opts = {
+      indent = {
+        char = "▏", -- A much thinner, more subtle character that blends into the background
+      },
+    },
+  },
 }
