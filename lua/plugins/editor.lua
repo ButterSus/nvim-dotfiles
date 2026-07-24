cc03281ab45c1db422fb0c7b94c82d6fbd4d99a5
Krivoshapkin Eduard <krivoshapkinea@proton.me>
2026-07-24 03:42:53 +0300

    feat: nvim-treesitter setup with default parsers
    Default parsers are installed only on non-vscode instance

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -3,6 +3,45 @@
 
 return {
   {
+    "nvim-treesitter/nvim-treesitter",
+    -- This plugin does not support lazy-loading.
+    lazy = false,
+    build = ":TSUpdate",
+    config = function()
+      require("nvim-treesitter").setup()
+
+      local parsers = {}
+
+      -- Default core parsers load ONLY when not in VS Code
+      if not vim.g.vscode then
+        parsers = {
+          "lua",
+          "vim",
+          "vimdoc",
+          "query",
+          "markdown",
+          "markdown_inline",
+          "bash",
+          "json",
+          "yaml",
+        }
+      end
+
+      -- Explicitly trigger parser installation programmatically for the main branch
+      if #parsers > 0 then
+        require("nvim-treesitter").install(parsers)
+      end
+
+      -- 3. Enable Native Neovim Highlighting & Indent
+      vim.api.nvim_create_autocmd("FileType", {
+        callback = function()
+          pcall(vim.treesitter.start)
+          vim.bo.indentexpr = "v:lua.vim.treesitter.indentexpr()"
+        end,
+      })
+    end,
+  },
+  {
     "matze/vim-move",
     cond = not vim.g.vscode,
     lazy = false,
