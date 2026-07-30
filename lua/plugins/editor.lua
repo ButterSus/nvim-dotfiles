d5ffc365df61c2b7910fd09e2a60af5a18c2859e
Krivoshapkin Eduard <krivoshapkinea@proton.me>
2026-07-31 01:12:10 +0300

    feat: more treesitter textobjects

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -10,6 +10,233 @@ return {
     dependencies = {
       "nvim-treesitter/nvim-treesitter-textobjects",
     },
+    keys = {
+      -- Functions
+      {
+        "af",
+        function()
+          require("nvim-treesitter-textobjects.select").select_textobject("@function.outer", "textobjects")
+        end,
+        mode = { "x", "o" },
+        desc = "Select Around Function",
+      },
+      {
+        "if",
+        function()
+          require("nvim-treesitter-textobjects.select").select_textobject("@function.inner", "textobjects")
+        end,
+        mode = { "x", "o" },
+        desc = "Select Inner Function",
+      },
+
+      -- Classes / structs
+      {
+        "ac",
+        function()
+          require("nvim-treesitter-textobjects.select").select_textobject("@class.outer", "textobjects")
+        end,
+        mode = { "x", "o" },
+        desc = "Select Around Class",
+      },
+      {
+        "ic",
+        function()
+          require("nvim-treesitter-textobjects.select").select_textobject("@class.inner", "textobjects")
+        end,
+        mode = { "x", "o" },
+        desc = "Select Inner Class",
+      },
+
+      -- Parameters / arguments
+      {
+        "aa",
+        function()
+          require("nvim-treesitter-textobjects.select").select_textobject("@parameter.outer", "textobjects")
+        end,
+        mode = { "x", "o" },
+        desc = "Select Around Argument",
+      },
+      {
+        "ia",
+        function()
+          require("nvim-treesitter-textobjects.select").select_textobject("@parameter.inner", "textobjects")
+        end,
+        mode = { "x", "o" },
+        desc = "Select Inner Argument",
+      },
+
+      -- Language scope (locals query)
+      {
+        "as",
+        function()
+          require("nvim-treesitter-textobjects.select").select_textobject("@local.scope", "locals")
+        end,
+        mode = { "x", "o" },
+        desc = "Select Language Scope",
+      },
+
+      -- Conditionals (if/else)
+      {
+        "ai",
+        function()
+          require("nvim-treesitter-textobjects.select").select_textobject("@conditional.outer", "textobjects")
+        end,
+        mode = { "x", "o" },
+        desc = "Select Around Conditional",
+      },
+      {
+        "ii",
+        function()
+          require("nvim-treesitter-textobjects.select").select_textobject("@conditional.inner", "textobjects")
+        end,
+        mode = { "x", "o" },
+        desc = "Select Inner Conditional",
+      },
+
+      -- Loops (for/while)
+      {
+        "al",
+        function()
+          require("nvim-treesitter-textobjects.select").select_textobject("@loop.outer", "textobjects")
+        end,
+        mode = { "x", "o" },
+        desc = "Select Around Loop",
+      },
+      {
+        "il",
+        function()
+          require("nvim-treesitter-textobjects.select").select_textobject("@loop.inner", "textobjects")
+        end,
+        mode = { "x", "o" },
+        desc = "Select Inner Loop",
+      },
+
+      -- Comments
+      {
+        "aC",
+        function()
+          require("nvim-treesitter-textobjects.select").select_textobject("@comment.outer", "textobjects")
+        end,
+        mode = { "x", "o" },
+        desc = "Select Around Comment",
+      },
+
+      -- Calls (function calls: foo(a, b))
+      {
+        "am",
+        function()
+          require("nvim-treesitter-textobjects.select").select_textobject("@call.outer", "textobjects")
+        end,
+        mode = { "x", "o" },
+        desc = "Select Around Call",
+      },
+      {
+        "im",
+        function()
+          require("nvim-treesitter-textobjects.select").select_textobject("@call.inner", "textobjects")
+        end,
+        mode = { "x", "o" },
+        desc = "Select Inner Call",
+      },
+
+      -- Return statements
+      {
+        "ar",
+        function()
+          require("nvim-treesitter-textobjects.select").select_textobject("@return.outer", "textobjects")
+        end,
+        mode = { "x", "o" },
+        desc = "Select Around Return",
+      },
+
+      -- Assignments (x = y;)
+      {
+        "a=",
+        function()
+          require("nvim-treesitter-textobjects.select").select_textobject("@assignment.outer", "textobjects")
+        end,
+        mode = { "x", "o" },
+        desc = "Select Around Assignment",
+      },
+      {
+        "i=",
+        function()
+          require("nvim-treesitter-textobjects.select").select_textobject("@assignment.inner", "textobjects")
+        end,
+        mode = { "x", "o" },
+        desc = "Select Inner Assignment (RHS)",
+      },
+
+      -- Movement: functions
+      {
+        "]f",
+        function()
+          require("nvim-treesitter-textobjects.move").goto_next_start("@function.outer", "textobjects")
+        end,
+        mode = { "n", "x", "o" },
+        desc = "Next Function Start",
+      },
+      {
+        "]F",
+        function()
+          require("nvim-treesitter-textobjects.move").goto_next_end("@function.outer", "textobjects")
+        end,
+        mode = { "n", "x", "o" },
+        desc = "Next Function End",
+      },
+      {
+        "[f",
+        function()
+          require("nvim-treesitter-textobjects.move").goto_previous_start("@function.outer", "textobjects")
+        end,
+        mode = { "n", "x", "o" },
+        desc = "Previous Function Start",
+      },
+      {
+        "[F",
+        function()
+          require("nvim-treesitter-textobjects.move").goto_previous_end("@function.outer", "textobjects")
+        end,
+        mode = { "n", "x", "o" },
+        desc = "Previous Function End",
+      },
+
+      -- Movement: classes / structs
+      {
+        "]c",
+        function()
+          require("nvim-treesitter-textobjects.move").goto_next_start("@class.outer", "textobjects")
+        end,
+        mode = { "n", "x", "o" },
+        desc = "Next Class/Struct Start",
+      },
+      {
+        "[c",
+        function()
+          require("nvim-treesitter-textobjects.move").goto_previous_start("@class.outer", "textobjects")
+        end,
+        mode = { "n", "x", "o" },
+        desc = "Previous Class/Struct Start",
+      },
+
+      -- Movement: parameters
+      {
+        "]a",
+        function()
+          require("nvim-treesitter-textobjects.move").goto_next_start("@parameter.inner", "textobjects")
+        end,
+        mode = { "n", "x", "o" },
+        desc = "Next Parameter",
+      },
+      {
+        "[a",
+        function()
+          require("nvim-treesitter-textobjects.move").goto_previous_start("@parameter.inner", "textobjects")
+        end,
+        mode = { "n", "x", "o" },
+        desc = "Previous Parameter",
+      },
+    },
     config = function()
       require("nvim-treesitter").setup()
 
@@ -59,32 +286,6 @@ return {
           include_surrounding_whitespace = false,
         },
       }
-
-      -- Textobjects keymaps
-      vim.keymap.set({ "x", "o" }, "af", function()
-        require("nvim-treesitter-textobjects.select").select_textobject("@function.outer", "textobjects")
-      end, { desc = "Select Around Function" })
-      vim.keymap.set({ "x", "o" }, "if", function()
-        require("nvim-treesitter-textobjects.select").select_textobject("@function.inner", "textobjects")
-      end, { desc = "Select Inner Function" })
-
-      vim.keymap.set({ "x", "o" }, "ac", function()
-        require("nvim-treesitter-textobjects.select").select_textobject("@class.outer", "textobjects")
-      end, { desc = "Select Around Class" })
-      vim.keymap.set({ "x", "o" }, "ic", function()
-        require("nvim-treesitter-textobjects.select").select_textobject("@class.inner", "textobjects")
-      end, { desc = "Select Inner Class" })
-
-      vim.keymap.set({ "x", "o" }, "aa", function()
-        require("nvim-treesitter-textobjects.select").select_textobject("@parameter.outer", "textobjects")
-      end, { desc = "Select Around Argument" })
-      vim.keymap.set({ "x", "o" }, "ia", function()
-        require("nvim-treesitter-textobjects.select").select_textobject("@parameter.inner", "textobjects")
-      end, { desc = "Select Inner Argument" })
-
-      vim.keymap.set({ "x", "o" }, "as", function()
-        require("nvim-treesitter-textobjects.select").select_textobject("@local.scope", "locals")
-      end, { desc = "Select Language Scope" })
     end,
   },
   {
